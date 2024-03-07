f47c8a39968c493ab80c68ebbfcd856cb912be71
Leop007 <perry.leonard@gmail.com>
2024-03-07 19:48:03 +0200

    commit

--- runApp.sh
+++ runApp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd ~
 python -m venv ~/venv/knowemployee
+timeout -5s
 source ~/env/knowemployee/bin/activate
 pip -y install deepgram-sdk Flask Flask-SQLAlchemy aiohttp Werkzeug PyJWT cryptography openai python-dotenv qrcode Pillow flask_migrate requests scrypt
 sudo apt -y install sqlite3 sqlitebrowser
