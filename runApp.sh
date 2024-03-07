40067b1c3abe876d0a891227a5c193cedc7d9f49
Leop007 <perry.leonard@gmail.com>
2024-03-08 00:54:31 +0200

    commit

--- runApp.sh
+++ runApp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-python -m venv ~/env/KnowEmployee
-sleep 3
-source ~/env/KnowEmployee/bin/activate
-pip  install deepgram-sdk Flask Flask-SQLAlchemy aiohttp Werkzeug PyJWT cryptography openai python-dotenv qrcode Pillow flask_migrate requests scrypt
-sudo apt install sqlite3 sqlitebrowser -y
-sudo apt autoremove -y
+sudo apt update -y # Update server to latest stable version
+
+git clone git@github.com:KnowEmploee/KnowEmploee.git # Clone KnowEmployee repository as local branch
+
+python -m venv ~/env/KnowEmployee # Create python virtual environment
+sleep 3 # it may take time, so let's give it 3 seconds :)
+source ~/env/KnowEmployee/bin/activate # Connect to python virtual environment
+
+pip  install deepgram-sdk Flask Flask-SQLAlchemy aiohttp Werkzeug PyJWT cryptography openai python-dotenv qrcode Pillow flask_migrate requests scrypt # Install needed dependencies 
+
+sudo apt install sqlite3 sqlitebrowser -y # We need SQLite
+sudo apt autoremove -y # Remove all tails (if there are any)
