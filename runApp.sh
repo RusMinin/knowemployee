018b64572d008d36394843add982695345808007
Leop007 <perry.leonard@gmail.com>
2024-03-08 11:52:21 +0200

    commit

--- runApp.sh
+++ runApp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt update -y # Update server to latest stable version
 
-git clone git@github.com:KnowEmploee/KnowEmploee.git # Clone KnowEmployee repository as local branch
+git clone git@github.com:KnowEmploee/knowemployee.git # Clone KnowEmployee repository as local branch
 
-python -m venv ~/env/KnowEmployee # Create python virtual environment
+python -m venv ~/env/knowemployee # Create python virtual environment
 sleep 3 # it may take time, so let's give it 3 seconds :)
-source ~/env/KnowEmployee/bin/activate # Connect to python virtual environment
+source ~/env/knowemployee/bin/activate # Connect to python virtual environment
 
 pip  install deepgram-sdk Flask Flask-SQLAlchemy aiohttp Werkzeug PyJWT cryptography openai python-dotenv qrcode Pillow flask_migrate requests scrypt gunicorn # Install needed dependencies 
 
