164f0b505bfd6fa42d29da40c86b99f584dab9cf
Leop007 <perry.leonard@gmail.com>
2024-03-17 19:53:55 +0200

    commit

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,6 @@
 # gunicorn.conf.py
 # Non logging stuff
+### Create directory and files in it and give it permission of user that is running the app as well, add log files to logrotatae ###
 bind = "0.0.0.0:80"
 workers = 3
 # Access log - records incoming HTTP requests
